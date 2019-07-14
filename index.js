d76f12b65c99aaef4e7f011d6c543e74754339e7
johnkalog <JohnKal97@gmail.com>
2019-07-14 11:04:22 +0300

    not the change

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import {store} from './lib/myStore.js'
 
 const render = () => {
   ReactDOM.render(
-    <Game />,
+    <Game store={store}/>,
     document.getElementById('root')
   );
 }
@@ -18,3 +18,12 @@ store.subscribe(render);
 store.dispatch({type:'newCards'});  //no render because state inisializes for first time with the dispatch
 
 //actioncreator
+//same fullo
+//collocate
+//oxi combineReducers
+//kathe presentationla exei container etsi mesw ths connect blepei mono auta pou ton endiaferoun
+//oti exeoun ta props
+//store createStore props h koino
+//idio id gia touw player1
+//() gia epistrofh objects
+//Provider
